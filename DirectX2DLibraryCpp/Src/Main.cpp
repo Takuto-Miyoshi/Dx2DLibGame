041274bf3c7cb83b2b114cb0d3bee4a9122db02d
Miyoshi_Takuto <miyoshi.taku10@gmail.com>
2020-08-06 11:35:42 +0900

    じゃんけん処理の追加 ・自分の手の入力 ・相手の手をランダムで抽選 ・自分と相手の手を表示 ・自分目線での結果を調べる ・自分目線での結果を表示 ・おまけ程度の背景を表示

--- DirectX2DLibraryCpp/Src/Main.cpp
+++ DirectX2DLibraryCpp/Src/Main.cpp
@@ -4,11 +4,29 @@
 #include <Windows.h>
 #include "Engine/Engine.h"
 #include "Common/Vec.h"
+#include <time.h>
+
+void PlayHandProcessing();
 
 
 Vec2 g_Position = Vec2(0.0f, 0.0f);
 Vec2 g_Scale = Vec2(1.0f, 1.0f);
 float g_Angle = 0.0f;
+
+int PlayerHand = 0;
+bool playHand = false;
+
+int ComHand = 0;
+
+int winner = 0;
+const int RESULT_DRAW = 0;
+const int RESULT_LOSE = 1;
+const int RESULT_WIN = 2;
+
+const int HAND_GU = 0;
+const int HAND_CHI = 1;
+const int HAND_PA = 2;
+
 
 // ゲーム処理
 void GameProcessing();
@@ -89,6 +107,24 @@ void GameProcessing()
 	//{
 	//	// キーが離された瞬間の処理
 	//}
+
+
+
+	if (playHand == false) {
+		if (Engine::IsKeyboardKeyPushed(DIK_1)) {
+			PlayerHand = HAND_GU;
+			PlayHandProcessing();
+		}
+		else if (Engine::IsKeyboardKeyPushed(DIK_2)) {
+			PlayerHand = HAND_CHI;
+			PlayHandProcessing();
+		}
+		else if (Engine::IsKeyboardKeyPushed(DIK_3)) {
+			PlayerHand = HAND_PA;
+			PlayHandProcessing();
+		}
+	}
+
 }
 
 void DrawProcessing()
@@ -96,13 +132,68 @@ void DrawProcessing()
 	// 描画開始
 	// 描画処理を実行する場合、必ず最初実行する
 	Engine::StartDrawing(0);
+	
+	Engine::LoadTexture("BackGround", "Res/GB.png");
+	Engine::DrawTexture(0, 0, "BackGround");
 
-	// フォント描画
-	Engine::DrawFont(0.0f, 0.0f, "FontSize:Small", FontSize::Small, FontColor::White);
-	Engine::DrawFont(0.0f, 30.0f, "FontSize:Regular", FontSize::Regular, FontColor::White);
-	Engine::DrawFont(0.0f, 60.0f, "FontSize:Large", FontSize::Large, FontColor::White);
+	if (playHand) {
+		switch (PlayerHand)
+		{
+		case 0:
+			Engine::LoadTexture("PlayerHandTexture", "Res/janken_gu.png");
+			break;
+		case 1:
+			Engine::LoadTexture("PlayerHandTexture", "Res/janken_choki.png");
+			break;
+		case 2:
+			Engine::LoadTexture("PlayerHandTexture", "Res/janken_pa.png");
+			break;
+		}
+
+		switch (ComHand)
+		{
+		case 0:
+			Engine::LoadTexture("ComHandTexture", "Res/janken_gu.png");
+			break;
+		case 1:
+			Engine::LoadTexture("ComHandTexture", "Res/janken_choki.png");
+			break;
+		case 2:
+			Engine::LoadTexture("ComHandTexture", "Res/janken_pa.png");
+			break;
+		}
+
+		switch (winner)
+		{
+		case 0:
+			Engine::DrawFont(320, 240, "DRAW", Large, Black);
+			break;
+		case 1:
+			Engine::DrawFont(320, 240, "LOSE...", Large, Black);
+			break;
+		case 2:
+			Engine::DrawFont(320, 240, "WIN!!", Large, Black);
+			break;
+		}
+
+		Engine::DrawTexture(220, 260, "PlayerHandTexture", 255, 0, 0.8f, 0.8f);
+		Engine::DrawTexture(220, 20, "ComHandTexture", 255, 180, 0.8f, 0.8f);
+	}
 
 	// 描画終了
 	// 描画処理を終了する場合、必ず最後に実行する
 	Engine::FinishDrawing();
+}
+
+void PlayHandProcessing() {
+	int winnerTemp = 0;
+	// 手を出したかのトリガー
+	playHand = true;
+
+	// コンピューターの手
+	srand((unsigned)time(NULL));
+	ComHand = rand() % 3;
+
+	// 結果
+	winner = (PlayerHand - ComHand + 3) % 3;
 }
